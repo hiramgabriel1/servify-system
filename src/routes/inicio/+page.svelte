fb928e311577dabdf9ae1442d1ed8f8356e94047
itsrusty <rusty3031@gmail.com>
2023-11-13 15:27:12 -0700

    test product

--- src/routes/inicio/+page.svelte
+++ src/routes/inicio/+page.svelte
@@ -117,22 +117,38 @@
           <div class="mb-6 sm:w-1/2 rounded-lg bg-white p-6">
             <div class="flex items-center justify-between">
               <div class="flex items-center">
-                <img class="mr-2 h-10 w-10 rounded-full object-cover" src="https://images.unsplash.com/photo-1566753323558-f4e0952af115?q=80&w=1921&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="profile" />
+                <img class="mr-2 h-10 w-10 rounded-full object-cover" src="https://th.bing.com/th/id/R.84d84462dd556d362a5902e1cd7c2f07?rik=Zd5vFPFPJ2uFKg&pid=ImgRaw&r=0" alt="product" />
                 <div>
                   <h3 class="text-base font-semibold text-gray-900">Café en las rocas</h3>
                   <span class="block text-xs font-normal text-gray-500"></span>
                 </div>
               </div>
-              <!-- button show product here -->
-              <button class="group relative h-12 w-28 overflow-hidden rounded-2xl bg-green-400 text-lg font-bold text-white">
-                Ver
-                <!-- <div class="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div> -->
-              </button>
-             
+                            <!-- button show product here -->
+              <button
+                class="inline-block rounded-full border border-indigo-600 bg-indigo-600 p-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                on:click={()=> console.log("listening click")}
+                >
+                <svg
+                    class="h-5 w-5 rtl:rotate-180"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    />
+                </svg>
+            </button>
             </div>
+             <!-- close best awards products -->
+
             <div class="mt-6 flex items-center justify-between text-sm font-semibold text-gray-900">
-                  <div class="flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-1 h-5 w-6 text-yellow-500">
+                <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-1 h-5 w-6 text-yellow-500">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                 </svg>
                 4,7 (750 Reviews)
@@ -140,30 +156,46 @@
             </div>
           </div>
         </div>
+
+        <!-- others products here -->
         <div class="px-4">
           <div class="my-5">
-            <h2 class="text-lg font-semibold text-gray-900">Mentors</h2>
+            <h2 class="text-lg font-semibold text-gray-900">Otros</h2>
           </div>
           <div class="grid sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3">
             <div class="mb-6 rounded-lg bg-white p-6">
               <div class="flex items-center justify-between">
                 <div class="flex items-center">
-                  <img class="mr-2 h-10 w-10 rounded-full object-cover" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="profile" />
+                <img class="mr-2 h-10 w-10 rounded-full object-cover" src="https://th.bing.com/th/id/R.84d84462dd556d362a5902e1cd7c2f07?rik=Zd5vFPFPJ2uFKg&pid=ImgRaw&r=0" alt="product" />
                   <div>
-                    <h3 class="text-base font-semibold text-gray-900">Alex Stanton</h3>
-                    <span class="block text-xs font-normal text-gray-500">UI / UX Designer</span>
+                    <h3 class="text-base font-semibold text-gray-900">Frappe Oreo</h3>
                   </div>
                 </div>
-                <p class="text-sm font-medium text-indigo-500"><span class="mr-0.5">+</span>Follow</p>
+                <!-- button show product here -->
+              <button
+              class="inline-block rounded-full border border-indigo-600 bg-indigo-600 p-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+              on:click={()=> console.log("listening click")}
+              >
+              <svg
+                  class="h-5 w-5 rtl:rotate-180"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+              >
+                  <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  />
+              </svg>
+          </button>
               </div>
-              <p class="my-6 text-sm font-normal text-gray-500">Hi, I'm Jessica Jane. I am a doctoral student at Harvard University majoring in Web . . .</p>
+              <p class="my-6 text-sm font-normal text-gray-500">
+                El frappé de Oreo es una bebida helada que combina la intensidad del chocolate, la textura de las galletas Oreo y la suavidad de la crema batida. ¡Perfecto para refrescarte! 🍪🥤
+              </p>
               <div class="mt-6 flex items-center justify-between text-sm font-semibold text-gray-900">
-                <div class="flex">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-2 h-5 w-5 text-base text-gray-500">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 6.878V6a2.25 2.25 0 012.25-2.25h7.5A2.25 2.25 0 0118 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 004.5 9v.878m13.5-3A2.25 2.25 0 0119.5 9v.878m0 0a2.246 2.246 0 00-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0121 12v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6c0-.98.626-1.813 1.5-2.122" />
-                  </svg>
-                  <span class="mr-1">40</span> Task
-                </div>
                 <div class="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-1 h-5 w-6 text-yellow-500">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
@@ -175,15 +207,37 @@
             <div class="mb-6 rounded-lg bg-white p-6">
               <div class="flex items-center justify-between">
                 <div class="flex items-center">
-                  <img class="mr-2 h-10 w-10 rounded-full object-cover" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="profile" />
+                    <img class="mr-2 h-10 w-10 rounded-full object-cover" src="https://th.bing.com/th/id/R.84d84462dd556d362a5902e1cd7c2f07?rik=Zd5vFPFPJ2uFKg&pid=ImgRaw&r=0" alt="product" />
                   <div>
-                    <h3 class="text-base font-semibold text-gray-900">Antoine</h3>
-                    <span class="block text-xs font-normal text-gray-500">Android Developer</span>
+                    <h3 class="text-base font-semibold text-gray-900">Mocca</h3>
+                    <!-- <span class="block text-xs font-normal text-gray-500">Android Developer</span> -->
                   </div>
                 </div>
-                <p class="text-sm font-medium text-indigo-500"><span class="mr-0.5">+</span>Follow</p>
-              </div>
-              <p class="my-6 text-sm font-normal text-gray-500">Hi, I'm Alex Stanton. I am a doctoral student at Oxford University majoring in UI / UX . . .</p>
+                <!-- button show product here -->
+                <button
+                class="inline-block rounded-full border border-indigo-600 bg-indigo-600 p-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                on:click={()=> console.log("listening click")}
+                >
+                <svg
+                    class="h-5 w-5 rtl:rotate-180"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    />
+                </svg>
+            </button>
+
+            </div>
+              <p class="my-6 text-sm font-normal text-gray-500">
+                Es para esos momentos en los que necesitas un capricho indulgente, cuando el mundo se mueve demasiado rápido y tú solo quieres detenerte un instante. Así que, ¿por qué no te sumerges en esta maravilla de sabor? 🍫☕
+              </p>
               <div class="mt-6 flex items-center justify-between text-sm font-semibold text-gray-900">
                 <div class="flex">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-2 h-5 w-5 text-base text-gray-500">
